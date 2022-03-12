68d39611e66242427ccf532d10bf95c506a8bd56
jaclynnbarrera <jaclynnbarrera@gmail.com>
2022-03-12 14:47:18 -0500

    create HeroSearch component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Top from "./top/Top";
+import HeroSearch from "./hero/HeroSearch";
 
 function App() {
   return (
     <div className="App">
       <Top />
+      <HeroSearch />
     </div>
   );
 }
---
+++ src/hero/HeroSearch.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HeroSearch.css";
+
+function heroSearch() {
+  return (
+    <div className="hero">
+      <h1>Shop locally to support your farmers and food growers</h1>
+      {/* search bar */}
+    </div>
+  );
+}
+
+export default heroSearch;
