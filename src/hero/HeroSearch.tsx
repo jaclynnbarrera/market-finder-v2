e03c8abb8114181d61fbe3e1083d638091717355
jaclynnbarrera <jaclynnbarrera@gmail.com>
2022-03-12 15:04:54 -0500

    upadte HeroSearch

--- src/hero/HeroSearch.tsx
+++ src/hero/HeroSearch.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./HeroSearch.css";
 
-function heroSearch() {
+function HeroSearch() {
   return (
     <div className="hero">
       <h1>Shop locally to support your farmers and food growers</h1>
-      {/* search bar */}
     </div>
   );
 }
 
-export default heroSearch;
+export default HeroSearch;
